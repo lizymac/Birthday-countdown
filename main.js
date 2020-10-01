9dddda4ff947bd445701cf64a9f39528158c2d0c
Elizabeth Mac <eliza.mac93@gmail.com>
2020-10-01 15:20:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+const birthdayTime = new Date(`January 12 ${currentYear + 1} 00:00:00`);
 
 //background year
 year.innerText = currentYear + 1;
 
 function updateCountdown() {
   const currentTime = new Date();
-  const diff = newYearTime - currentTime;
+  const diff = birthdayTime - currentTime;
 
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
